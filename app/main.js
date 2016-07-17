b5aaab6bec6c158efe8ab4102d3ad63c1d47109d
Tyler Stinson <tjstinso@gmail.com>
2016-07-17 11:54:48 -0700

    able to draw entire map to canvas

--- app/main.js
+++ app/main.js
@@ -4,12 +4,14 @@ import { Map, Piece } from './map';
 
   let ele = document.getElementById('map');
   let ctx = document.getElementById('map').getContext('2d');
-  let map = new Map(ctx, {x: 50, y: 50}, 500);
-  ele.width = `${map.width}`
-  ele.height = `${map.width}`;
+  let map = new Map(ctx, {x: 50, y: 50}, 50);
+  ele.width = `${500}`
+  ele.height = `${500}`;
 
-  //map.drawMap();
+  map.drawMap();
 
-  Piece.drawHex(ctx, {x: 50, y: 50}, 50);
+  //Piece.drawHex(ctx, {x: 50, y: 50}, 50);
+  //Piece.drawHex(ctx, {x: 50, y: 50 + Math.sqrt(3) * 50}, 50);
+  //Piece.drawHex(ctx, {x: 50, y: 50 + 2 * Math.sqrt(3) * 50}, 50);
 
 })()
--- app/map.js
+++ app/map.js
@@ -5,33 +5,50 @@ export class Map {
     this.context = context;
     this.origin = origin;
     this.ratio = width / 2;
-    this.pieceHeight = width * 2 * Math.cos(Math.PI / 180 * 30);
+    this.pieceHeight = width * Math.sqrt(3);
+  }
+
+  calcY(xOffset, yOffset) {
+    let y = this.origin.y + yOffset * Math.sqrt(3) * this.width;
+    switch (xOffset) {
+      case 0:
+        return y + this.pieceHeight;
+      case 1:
+        return y + this.pieceHeight / 2;
+      case 2:
+        return y;
+    }
+  }
+
+  calcX(xOffset) {
+    return this.origin.x + xOffset * this.width * (3/2);
   }
 
   drawMap() {
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 3; i++) {
+      //Piece.drawHex(this.context, {x: this.origin.x, y: this.origin.y}, this.width)
       if (i === 0) {
+        let x_1 = this.calcX(i);
+        let x_2 = this.calcX(4);
         for (let j = 0; j < 3; j++) {
-          let x = i * this.width;
-          let y = (1 + j) * this.pieceHieght;
-          console.log(x);
-          console.log(y);
-          Piece.drawHex(this.context, {x, y}, this.width)
+          let y = this.calcY(i, j)
+          Piece.drawHex(this.context, {x: x_1, y}, this.width)
+          Piece.drawHex(this.context, {x: x_2, y}, this.width)
         }
       } else if (i === 1) {
-        if (i != 5) {
-
-        }
-      } else if (i === 2) {
-
-      } else if (i === 3) {
-        if (i != 5) {
-
+        let x_1 = this.calcX(i);
+        let x_2 = this.calcX(3);
+        for (let j = 0; j < 4; j++) {
+          let y = this.calcY(i, j);
+          Piece.drawHex(this.context, {x: x_1, y}, this.width)
+          Piece.drawHex(this.context, {x: x_2, y}, this.width)
         }
       } else {
-        if (i != 0 && i != 4) {
-
+        let x = this.calcX(i);
+        for (let j = 0; j < 5; j++) {
+          let y = this.calcY(i, j)
+          Piece.drawHex(this.context, {x, y}, this.width)
         }
       }
     }
@@ -41,22 +58,19 @@ export class Map {
 }
 
 export class Piece {
-  constructor(context, origin, width) {
-    this.context = context;
-    this.origin = origin;
-    this.width = width;
-  }
 
   static drawHex(context, origin, width) {
     let ratio = width / 2;
+    let offset = ratio * Math.sqrt(3);
     context.beginPath();
-    context.moveTo(origin.x + ratio, origin.y + width * Math.sqrt(3) / 2);
+    context.moveTo(origin.x + ratio, origin.y + offset);
     context.lineTo(origin.x + width, origin.y);
-    context.lineTo(origin.x + ratio, origin.y - width * Math.sqrt(3) / 2);
-    context.lineTo(origin.x - ratio, origin.y - width * Math.sqrt(3) / 2);
+    context.lineTo(origin.x + ratio, origin.y - offset);
+    context.lineTo(origin.x - ratio, origin.y - offset);
     context.lineTo(origin.x - width, origin.y);
-    context.lineTo(origin.x - ratio, origin.y + width * Math.sqrt(3) / 2);
-    context.lineTo(origin.x + ratio, origin.y + width * Math.sqrt(3) / 2);
+    context.lineTo(origin.x - ratio, origin.y + offset);
+    context.lineTo(origin.x + ratio, origin.y + offset);
     context.fill()
   }
+
 }
